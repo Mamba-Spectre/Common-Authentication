40ea359bdee431863fc5389eda1c8728f3ccc7d1
Harsh <harshvardhan23.007@gmail.com>
2024-02-18 22:17:26 +0530

    working

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,38 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
+// User Config
 const UserSchema = new mongoose.Schema({
-    username: {type:String,required:true},
-    email:{type:String,required:true},
-    authentication:{
-        password:{type:String,required:true, select:false},
-        salt:{type:String,select:false},
-        sessionToken:{type:String,select:false},
-    },
+  email: { type: String, required: true },
+  username: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
 });
 
-export const UserModal = mongoose.model('User',UserSchema);
+export const UserModel = mongoose.model('User', UserSchema);
 
-
-export const getUsers = () =>{
-    UserModal.find();
-}
-export const getUserByEmail = (email: String) =>{
-    return UserModal.findOne({email});
-}
-export const getUserById = (id:string) => {
-    UserModal.findById(id);
-}
-export const getUserBySessionToken = (sessionToken: string)=>{
-    UserModal.findOne({
-        'authentication.sessionToken':sessionToken
-    })
-}
-export const createUser = (values: Record<string,any>) => {
-    new UserModal(values);
-}
-export const deleteUsetById = (id:string) => {
-    UserModal.findOneAndDelete({_id:id});
-}
-export const updateUserById = (id:string, values: Record<string,any>) => {
-    UserModal.findByIdAndUpdate(id,values)
-;}
+// User Actions
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,33 @@
 import express from 'express';
-// import http from 'http';
+import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-import mongoose from 'mongoose';
+
 import router from './router';
-const http = require('http');
+import mongoose from 'mongoose';
+
 const app = express();
 
-app.use(cors({credentials: true}));
+app.use(cors({
+  credentials: true,
+}));
+
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-
-server.listen(8080,()=>{
-    console.log("8080 it is")
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080/');
 });
 
-
-const MONGO_URL = "mongodb+srv://harshvardhan23007:3gtQgfiUUd1z0fiQ@cluster0.xgklla1.mongodb.net/?retryWrites=true&w=majority"
-
+const MONGO_URL = 'mongodb+srv://harshvardhan23007:3gtQgfiUUd1z0fiQ@cluster0.xgklla1.mongodb.net/?retryWrites=true&w=majority'; // DB URI
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
-mongoose.connection.on('error',(error: Error) => console.log(error));
-app.use('/',router());
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
